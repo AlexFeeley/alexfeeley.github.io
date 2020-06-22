e69cac1f015d84cac3ce7bccbae354804931e655
Alex Feeley <alexandra.feeley.lamb@vanderbilt.edu>
2020-06-21 23:52:16 -0400

    Fixed single page layout

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// Landing Page Script
+VANTA.BIRDS({
+    el: "#vantajs",
+    mouseControls: true,
+    touchControls: true,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x282828,
+    color1: 0x350052,
+    color2: 0x123c69,
+    colorMode: "lerp",
+    birdSize: 1.50,
+    quantity: 3,
+    separation: 15,
+    cohesion: 50
+});
+
+// Navigation Bar
+function navBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
